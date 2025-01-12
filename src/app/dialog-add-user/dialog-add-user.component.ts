352fbeae5c099dd042e85aa61302a9dab9a384a9
M1roel <ppfautsch@yahoo.de>
2025-01-12 17:43:34 +0100

    Enhance user addition dialog with form fields for first name, last name, and date selection; update component imports and styles for improved layout

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogModule],
+  imports: [
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
   templateUrl: './dialog-add-user.component.html',
-  styleUrl: './dialog-add-user.component.scss'
+  styleUrl: './dialog-add-user.component.scss',
 })
-export class DialogAddUserComponent {
-
-}
+export class DialogAddUserComponent {}
