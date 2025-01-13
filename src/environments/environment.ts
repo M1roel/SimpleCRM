603adbf6e7162e1e493e2711f9b00655cf7ca868
M1roel <ppfautsch@yahoo.de>
2025-01-13 13:19:41 +0000

    try to implement firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyCFgfk6hu17koCrGpb-9zW-FUUhM_sGWXQ",
+      authDomain: "simple-crm-f58d7.firebaseapp.com",
+      projectId: "simple-crm-f58d7",
+      storageBucket: "simple-crm-f58d7.firebasestorage.app",
+      messagingSenderId: "441894599370",
+      appId: "1:441894599370:web:90a1f926e404b49d0a1233"
+    }
+  };
+  
