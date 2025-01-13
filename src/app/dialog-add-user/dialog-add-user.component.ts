163878bd0456fb909e709034ecb6680b36772bd2
M1roel <ppfautsch@yahoo.de>
2025-01-13 22:10:09 +0100

    add loading indicator and disable inputs during user addition

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
@@ -8,17 +9,20 @@ import { FormsModule } from '@angular/forms';
 import { User } from '../models/user.class';
 import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 import { Inject } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
   imports: [
+    CommonModule,
     MatDialogModule,
     MatInputModule,
     MatFormFieldModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    FormsModule
+    FormsModule,
+    MatProgressBarModule
   ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
@@ -26,6 +30,7 @@ import { Inject } from '@angular/core';
 
 export class DialogAddUserComponent {
 
+  loading = false;
   user = new User;
   birthDate: Date = new Date;
   constructor(@Inject(Firestore) private firestore: Firestore) {}
@@ -36,11 +41,13 @@ export class DialogAddUserComponent {
     const userData = { ...this.user };
   
     console.log('Current user is', userData);
+    this.loading = true;
   
     const usersCollection = collection(this.firestore, 'users');
     addDoc(usersCollection, userData)
       .then((result) => {
         console.log('Adding user finished', result);
+        this.loading = false;
       })
       .catch((error) => {
         console.error('Error adding user:', error);
