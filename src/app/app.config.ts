f9dc769e60c59112cdbd03432c946349b3a7658e
M1roel <ppfautsch@yahoo.de>
2025-01-13 14:48:00 +0000

    second try to implement firebase and firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "simple-crm-f58d7", "appId": "1:441894599370:web:90a1f926e404b49d0a1233", "storageBucket": "simple-crm-f58d7.firebasestorage.app", "apiKey": "AIzaSyCFgfk6hu17koCrGpb-9zW-FUUhM_sGWXQ", "authDomain": "simple-crm-f58d7.firebaseapp.com", "messagingSenderId": "441894599370" }))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
