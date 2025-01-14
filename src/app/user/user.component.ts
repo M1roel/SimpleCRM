afa2a54d20ea44078bb8482c2456fd09119ce1f0
M1roel <ppfautsch@yahoo.de>
2025-01-14 22:06:15 +0100

    feat: add user detail component and routing for user profile

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,14 +5,17 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../models/user.class';
 import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
 
+
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, MatCardModule, CommonModule],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, MatCardModule, CommonModule,
+    RouterModule],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
