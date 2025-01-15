399bda80bcb74861ba6d6a98ae04bcaca01ab3be
M1roel <ppfautsch@yahoo.de>
2025-01-15 14:15:30 +0000

    feat: get user data from firestore for user-detail component with unique id

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
 import { MatCardModule } from '@angular/material/card';
+import { User } from '../models/user.class';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
   imports: [MatCardModule],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+  styleUrls: ['./user-detail.component.scss']
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
 
+  userId = '';
+  user: User = new User();
+
+  constructor(
+    public dialog: MatDialog, 
+    @Inject(Firestore) private firestore: Firestore, 
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(paramMap => {
+      this.userId = paramMap.get('id') || '';
+      console.log('GOT ID', this.userId);
+      this.getUser();
+    });
+  }
+
+  getUser() {
+    if (this.userId) {
+      const userDocRef = doc(this.firestore, `users/${this.userId}`);
+      docData(userDocRef).subscribe((user: any) => {
+        this.user = new User(user);
+      });
+    } else {
+      console.error('No userId found');
+    }
+  }
 }
