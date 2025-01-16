ffc6ee21bc1e7c9a235f7599de6ff4d9beb9e4a8
M1roel <ppfautsch@yahoo.de>
2025-01-16 09:43:21 +0100

    feat: translate modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -22,11 +24,44 @@ import { MatMenuModule } from '@angular/material/menu';
     MatButtonModule,
     MatNativeDateModule,
     MatCardModule,
-    MatMenuModule
+    MatMenuModule,
+    TranslateModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'SimpleCRM';
+
+  /**
+   * Initializes the translation service and configures the default language.
+   * Automatically detects and applies the user's preferred language if supported.
+   *
+   * @param {TranslateService} translate - Service for managing translations and languages.
+   */
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
+
+  /**
+   * Switches the language of the application using the TranslateService.
+   * 
+   * @param {Event} event The event object triggered by the language switch.
+   * @param {string} language The language to switch to.
+   */
+  useLanguage(event: Event, language: string): void {
+    event.preventDefault();
+    this.translate.use(language);
+  }
+
+  /**
+   * Retrieves the current language for display or further processing.
+   * 
+   * @returns {string} The current language of the application.
+   */
+  get currentLanguage(): string {
+    return this.translate.currentLang;
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,27 +1,58 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
+/**
+ * Factory function to create a TranslateHttpLoader instance.
+ * This loader is responsible for loading translation files from the specified path.
+ *
+ * @param {HttpClient} http - The HttpClient instance used to make HTTP requests.
+ * @returns {TranslateHttpLoader} - A new instance of TranslateHttpLoader configured with the translation file path and extension.
+ */
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (
+  http: HttpClient
+) => new TranslateHttpLoader(http, './assets/i18n/', '.json');
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
-  provideAnimationsAsync(),
-  importProvidersFrom(
-    provideFirebaseApp(() =>
-      initializeApp({
-        "projectId": "simple-crm-f58d7",
-        "appId": "1:441894599370:web:90a1f926e404b49d0a1233",
-        "storageBucket": "simple-crm-f58d7.firebasestorage.app",
-        "apiKey": "AIzaSyCFgfk6hu17koCrGpb-9zW-FUUhM_sGWXQ",
-        "authDomain": "simple-crm-f58d7.firebaseapp.com",
-        "messagingSenderId": "441894599370"
-      }))),
-  importProvidersFrom(provideAuth(() =>
-    getAuth())),
-  importProvidersFrom(provideFirestore(() =>
-    getFirestore()))]
+  providers: [
+    provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'simple-crm-f58d7',
+          appId: '1:441894599370:web:90a1f926e404b49d0a1233',
+          storageBucket: 'simple-crm-f58d7.firebasestorage.app',
+          apiKey: 'AIzaSyCFgfk6hu17koCrGpb-9zW-FUUhM_sGWXQ',
+          authDomain: 'simple-crm-f58d7.firebaseapp.com',
+          messagingSenderId: '441894599370',
+        })
+      )
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom([TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+    })])
+  ],
 };
