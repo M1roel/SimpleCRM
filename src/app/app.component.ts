d0789a1c32c21ad9b76ac8bb4301292d6ee3ad72
M1roel <ppfautsch@yahoo.de>
2025-01-19 18:56:51 +0100

    feat: new language change design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -10,6 +10,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslateService } from "@ngx-translate/core";
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-root',
@@ -26,6 +27,7 @@ import { TranslateService } from "@ngx-translate/core";
     MatCardModule,
     MatMenuModule,
     TranslateModule,
+    MatButtonToggleModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -33,35 +35,17 @@ import { TranslateService } from "@ngx-translate/core";
 export class AppComponent {
   title = 'SimpleCRM';
 
-  /**
-   * Initializes the translation service and configures the default language.
-   * Automatically detects and applies the user's preferred language if supported.
-   *
-   * @param {TranslateService} translate - Service for managing translations and languages.
-   */
+  currentLanguage = signal('en');
+
   constructor(private translate: TranslateService) {
     this.translate.addLangs(['de', 'en']);
     this.translate.setDefaultLang('en');
-    this.translate.use('en');
+    this.translate.use(this.currentLanguage());
   }
 
-  /**
-   * Switches the language of the application using the TranslateService.
-   * 
-   * @param {Event} event The event object triggered by the language switch.
-   * @param {string} language The language to switch to.
-   */
-  useLanguage(event: Event, language: string): void {
-    event.preventDefault();
-    this.translate.use(language);
-  }
-
-  /**
-   * Retrieves the current language for display or further processing.
-   * 
-   * @returns {string} The current language of the application.
-   */
-  get currentLanguage(): string {
-    return this.translate.currentLang;
+  // Methode zum Umschalten der Sprache
+  useLanguage(language: string): void {
+    this.currentLanguage.set(language); // Setzt das Signal
+    this.translate.use(language); // Aktualisiert die Übersetzung
   }
 }
