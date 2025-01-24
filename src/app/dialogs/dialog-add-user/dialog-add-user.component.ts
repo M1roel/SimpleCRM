63a76cb12b5d3a2ed78b9b8ed126fc55e3f93208
M1roel <ppfautsch@yahoo.de>
2025-01-24 08:35:26 +0000

    feat: assign user roles, like doctor, nurse, controller, etc.

--- src/app/dialogs/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialogs/dialog-add-user/dialog-add-user.component.ts
@@ -1,16 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatDialogModule, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSelectModule } from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
 import { User } from '../../models/user.class';
 import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 import { Inject } from '@angular/core';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+interface Role {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -24,27 +30,51 @@ import { TranslateModule } from '@ngx-translate/core';
     MatNativeDateModule,
     FormsModule,
     MatProgressBarModule,
-    TranslateModule
+    TranslateModule,
+    MatSelectModule
   ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
 })
-
 export class DialogAddUserComponent {
 
   loading = false;
   user = new User;
-  birthDate: Date = new Date;
-  constructor(@Inject(Firestore) private firestore: Firestore, public dialogRef: MatDialogRef<DialogAddUserComponent>) {}
+  birthDate: Date = new Date();
+  roles: Role[] = [];
+  selectedRole: string = '';
+
+  constructor(
+    @Inject(Firestore) private firestore: Firestore,
+    public dialogRef: MatDialogRef<DialogAddUserComponent>,
+    private translate: TranslateService
+  ) {
+    this.initializeRoles();
+  }
+
+  private initializeRoles(): void {
+    this.roles = [
+      { value: 'doctor', viewValue: this.translate.instant('ADD_USER.ROLES.DOCTOR') },
+      { value: 'nurse', viewValue: this.translate.instant('ADD_USER.ROLES.NURSE') },
+      { value: 'controller', viewValue: this.translate.instant('ADD_USER.ROLES.CONTROLLER') },
+      { value: 'aemp', viewValue: this.translate.instant('ADD_USER.ROLES.AEMP') },
+      { value: 'op-manager', viewValue: this.translate.instant('ADD_USER.ROLES.OP_MANAGER') },
+      { value: 'admin-staff', viewValue: this.translate.instant('ADD_USER.ROLES.ADMIN_STAFF') },
+      { value: 'it-support', viewValue: this.translate.instant('ADD_USER.ROLES.IT_SUPPORT') },
+      { value: 'quality-manager', viewValue: this.translate.instant('ADD_USER.ROLES.QUALITY_MANAGER') },
+      { value: 'physiotherapist', viewValue: this.translate.instant('ADD_USER.ROLES.PHYSIOTHERAPIST') },
+      { value: 'lab-technician', viewValue: this.translate.instant('ADD_USER.ROLES.LAB_TECHNICIAN') }
+    ];
+    this.selectedRole = this.roles[0]?.value; // Standardmäßig die erste Rolle auswählen
+  }
 
   saveUser() {
     this.user.birthDate = this.birthDate.getTime();
-  
-    const userData = { ...this.user };
-  
+    const userData = { ...this.user, role: this.selectedRole };
+
     console.log('Current user is', userData);
     this.loading = true;
-  
+
     const usersCollection = collection(this.firestore, 'users');
     addDoc(usersCollection, userData)
       .then((result) => {
@@ -54,6 +84,11 @@ export class DialogAddUserComponent {
       })
       .catch((error) => {
         console.error('Error adding user:', error);
+        this.loading = false;
       });
+  }
+
+  selectRole(event: Event) {
+    this.selectedRole = (event.target as HTMLSelectElement).value;
   }
 }
